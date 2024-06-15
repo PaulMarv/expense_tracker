441c57be376d36d657a049a7d06a0e4e9ea8a050
PaulMarv <mpsalako@gmail.com>
2024-06-15 15:01:55 +0100

    bar chart ui

--- lib/screen/stats/stats.dart
+++ lib/screen/stats/stats.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/screen/stats/chart.dart';
 import 'package:flutter/material.dart';
 
 class StatsScreen extends StatelessWidget {
@@ -5,6 +6,34 @@ class StatsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Transactions',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(12, 20, 12, 12),
+                child: MyChart(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
