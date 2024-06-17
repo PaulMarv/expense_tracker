302ad25c8d67d1e68be8a78fed37ac9d14e8fd0f
PaulMarv <mpsalako@gmail.com>
2024-06-17 18:26:17 +0100

    navigation for add page

--- lib/screen/home/views/home_screen.dart
+++ lib/screen/home/views/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:expense_tracker/screen/home/views/main_screen.dart';
 import 'package:expense_tracker/screen/stats/stats.dart';
+import 'package:expense_tracker/screen/add_expense/views/add_expense.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,6 @@ class _HomeScreenState extends State<HomeScreen> {
   int index = 0;
   late Color selectedItem = Colors.blue;
   Color unselectedItem = Colors.grey;
-
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +50,14 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => const AddExpense(),
+              ),
+            );
+          },
           shape: const CircleBorder(),
           child: Container(
               width: 60,
