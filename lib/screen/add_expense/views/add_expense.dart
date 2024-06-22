367a72fd82987da143e908d9ee1abe899c87b56c
PaulMarv <mpsalako@gmail.com>
2024-06-22 18:33:07 +0100

    input fields added

--- lib/screen/add_expense/views/add_expense.dart
+++ lib/screen/add_expense/views/add_expense.dart
@@ -1,11 +1,57 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AddExpense extends StatelessWidget {
   const AddExpense({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Add Expenses',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.dollarSign,
+                      size: 16,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(),
+              TextButton(onPressed: () {}, child: const Text('Save'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
