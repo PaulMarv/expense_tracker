0016b203f04bb959f600442346d07e2857722e1b
PaulMarv <mpsalako@gmail.com>
2024-06-29 19:50:06 +0100

    added date selector

--- lib/screen/add_expense/views/add_expense.dart
+++ lib/screen/add_expense/views/add_expense.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class AddExpense extends StatelessWidget {
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
 
+  @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -28,29 +36,90 @@ class AddExpense extends StatelessWidget {
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.7,
                 child: TextFormField(
+                  controller: expenseController,
+                  textAlignVertical: TextAlignVertical.center,
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.white,
                     prefixIcon: const Icon(
                       FontAwesomeIcons.dollarSign,
                       size: 16,
+                      color: Colors.grey,
                     ),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none
-                    ),
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none),
                   ),
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              TextFormField(
+                controller: categoryController,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.list,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  hintText: 'Category',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
                 ),
               ),
               const SizedBox(
                 height: 16,
               ),
-              TextFormField(),
-              const SizedBox(
-                height: 16,
+              TextFormField(
+                controller: dateController,
+                textAlignVertical: TextAlignVertical.center,
+                readOnly: true,
+                onTap: () {
+                  showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime.now().add(const Duration(days: 365)));
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.clock,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  hintText: 'Date',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
               ),
-              TextFormField(),
-              TextButton(onPressed: () {}, child: const Text('Save'))
+              const SizedBox(
+                height: 32,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        backgroundColor: Colors.black),
+                    onPressed: () {},
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                    )),
+              )
             ],
           ),
         ),
